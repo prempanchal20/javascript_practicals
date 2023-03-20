326da373d09e58c887f2614e6c5bb5955d263765
prem <prem.panchal@simformsolutions.com>
2023-03-20 19:22:38 +0530

    add backspace functionality

--- scientific_calculator/script.js
+++ scientific_calculator/script.js
@@ -1,12 +1,12 @@
 let input = document.querySelector("input");
 let buttons = document.querySelectorAll("button");
+const backSpace = document.getElementById("backSpace");
 
 // Button Click Event
 buttons.forEach((btn) =>
   btn.addEventListener("click", (event) => {
     console.log(event.target.innerText);
     // console.log(event);
-    input.value += event.target.innerText;
     // let operators = ["+", "-", "*", "/", "x"];
     // switch (event.target.innerText) {
     //   case "=":
@@ -21,6 +21,13 @@ buttons.forEach((btn) =>
       console.log(newVal);
       console.log(eval(newVal));
       input.value = eval(newVal);
+    } else if (event.target.innerText === "") {
+      console.log("ckdisn");
+      let backSpace = inputVal.substring(0, inputVal.length - 1);
+      console.log(backSpace);
+      input.value = backSpace;
+    } else {
+      input.value += event.target.innerText;
     }
   })
 );
