0d245e5154527ff82d21f915c04f92e9f9e454f8
prem <prem.panchal@simformsolutions.com>
2023-03-21 11:07:52 +0530

    remove comments from code

--- scientific_calculator/script.js
+++ scientific_calculator/script.js
@@ -1,28 +1,18 @@
 let input = document.querySelector("input");
 let buttons = document.querySelectorAll("button");
-const backSpace = document.getElementById("backSpace");
+let backSpace = document.getElementById("backSpace");
 
 // Button Click Event
 buttons.forEach((btn) =>
   btn.addEventListener("click", (event) => {
     console.log(event.target.innerText);
-    // console.log(event);
-    // let operators = ["+", "-", "*", "/", "x"];
-    // switch (event.target.innerText) {
-    //   case "=":
-    //     // input.value=eval(input.value)
-    //     // let result = eval(input.value);
-    //     console.log(eval(input.value));
-    // }
     let inputVal = input.value;
     if (event.target.innerText === "=") {
       console.log(input.value);
-      let newVal = inputVal.substring(0, inputVal.length - 1);
-      console.log(newVal);
-      console.log(eval(newVal));
-      input.value = eval(newVal);
+    //   console.log(newVal);
+      console.log(eval(input.value));
+      input.value = eval(input.value);
     } else if (event.target.innerText === "") {
-      console.log("ckdisn");
       let backSpace = inputVal.substring(0, inputVal.length - 1);
       console.log(backSpace);
       input.value = backSpace;
