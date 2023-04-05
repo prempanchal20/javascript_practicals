bac251fdd82fbf21ed3d37ecd740cc80e813ac9f
prem <prem.panchal@simformsolutions.com>
2023-04-05 11:51:41 +0530

    add values in localstorage

--- CRUD/script.js
+++ CRUD/script.js
@@ -1,22 +1,40 @@
 let submitBtn = document.getElementById("submit");
+let productID = document.getElementById("productID");
+let productName = document.getElementById("productName");
+let productImage = document.getElementById("productImage");
+let productPrice = document.getElementById("productPrice");
+let productDescription = document.getElementById("productDescription");
 
 // Submit Button Event
 submitBtn.addEventListener("click", (event) => {
   event.preventDefault(); // Hold the Page after Submit button clicked
-  console.log("btn clicked");
+
+  // Get values from Input
+  let productIDValue = productID.value;
+  let productNameValue = productName.value;
+  let productImageValue = productImage.value;
+  let productPriceValue = productPrice.value;
+  let productDescriptionValue = productDescription.value;
+
+  let keyValue = {
+    productID: productIDValue,
+    productName: productNameValue,
+    productImage: productImageValue,
+    productPrice: productPriceValue,
+    productDescription: productDescriptionValue,
+  };
+
+  // console.log("keyValue is:- ", keyValue);
+  // console.log(Object.keys(keyValue));
+  // console.log(Object.values(keyValue));
+
+  //Store Values In Localstorage
+  let data = [];
+  data.push({
+    productID: productIDValue,
+    productName: productNameValue,
+    productImage: productImageValue,
+  });
+  localStorage.setItem("data", JSON.stringify(data));
+  console.log(data);
 });
-
-let setValues = window.localStorage.setItem(
-  "productID",
-  "productName",
-  "productImage",
-  "productPrice",
-  "productDescription"
-);
-
-// Set Values In Array
-setValuesArray = setValues.toArray();
-console.log("Set Values", setValues);
-
-let setValuesArray = setValues.split(",");
-console.log("Set Values Array- ", setValuesArray);
