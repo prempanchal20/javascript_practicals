31e53d0820f5261b08d8d565a5db99e49c72d26b
prem <prem.panchal@simformsolutions.com>
2023-04-10 15:23:43 +0530

    added edit image functionality

--- CRUD/product.js
+++ CRUD/product.js
@@ -1,8 +1,10 @@
 let productID_modal = document.querySelector("#productID_modal");
-let productName_modal = document.querySelector("#productName_modal")
+let productName_modal = document.querySelector("#productName_modal");
 let productPrice_modal = document.querySelector("#productPrice_modal");
 let productImage_modal = document.querySelector("#productImage_modal");
-let productDescription_modal = document.querySelector("#productDescription_modal");
+let productDescription_modal = document.querySelector(
+  "#productDescription_modal"
+);
 let addEditProduct = document.querySelector(".addEditProduct");
 
 // Global State for Local Storage Data
@@ -12,7 +14,7 @@ let setDataToLocalStorage =
 let pid_modal = document.getElementById("productID_modal");
 let pname_modal = document.getElementById("productName_modal");
 let pprice_modal = document.getElementById("productPrice_modal");
-let pimage_modal = document.getElementById("productImage_modal")
+let pimage_modal = document.getElementById("productImage_modal");
 let pdesc_modal = document.getElementById("productDescription_modal");
 
 // Add Product Functionality
@@ -21,7 +23,7 @@ cardContainer.innerHTML = "";
 setDataToLocalStorage.forEach((element) => {
   cardContainer.innerHTML += `<div class="col-12 col-md-6 col-lg-4">
 <div class="card" id="${element.productID}">
-  <img src="Images/Iphone.png" class="card-img-top" alt="Iphone" />
+  <img src="${element.productImage}" class="card-img-top" alt="Image" />
   <div class="card-body">
     <h5 class="card-title">${element.productName}</h5>
     <p class="card-text">
@@ -46,21 +48,36 @@ function updateData(pID) {
   productID_modal.value = updatedData.productID;
   productName_modal.value = updatedData.productName;
   productPrice_modal.value = updatedData.productPrice;
-  productImage_modal.value = updatedData.productImage
   productDescription_modal.value = updatedData.productDescription;
 
-  addEditProduct.addEventListener('click', () => {
+  // Update Image in Edit Functionality
+  let editImage;
+  productImage_modal.addEventListener("change", (event) => {
+    const image = event.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(image);
+    reader.addEventListener("load", () => {
+      editImage = reader.result;
+    });
+  });
+  console.log(productImage_modal);
+
+  addEditProduct.addEventListener("click", () => {
     updatedData.productName = productName_modal.value;
-    console.log(updatedData);
 
     // Store Updated Data in Local Storage
     let index = setDataToLocalStorage.indexOf(updatedData);
     setDataToLocalStorage[index].productName = productName_modal.value;
     setDataToLocalStorage[index].productPrice = productPrice_modal.value;
-    setDataToLocalStorage[index].productImage = productImage_modal.value;
-    setDataToLocalStorage[index].productDescription = productDescription_modal.value;
-    localStorage.setItem("localStorageData", JSON.stringify(setDataToLocalStorage));
-  })
+    setDataToLocalStorage[index].productImage = editImage;
+    setDataToLocalStorage[index].productDescription =
+      productDescription_modal.value;
+
+    localStorage.setItem(
+      "localStorageData",
+      JSON.stringify(setDataToLocalStorage)
+    );
+  });
 }
 
 // Delete Data Functionality
@@ -74,24 +91,8 @@ function removeProduct(e) {
   localStorage.setItem("localStorageData", JSON.stringify(updatedData));
 }
 
-
-
-
-
-
-
-
-// if (setDataToLocalStorage.length = 0) {
-//   location.replace("empty_cart.html");
-// } else {
-//   location.replace('product.html')
-// }
-
-
-
-// Dynamic Image - First Store Image in Local Storage
 // Sort and Filter
 // Empty Cart - Image Show
 // Same ID Not Allow
-// Alert Box
+// Alert Box before delete and edit
 // CSS Media Query
--- CRUD/script.js
+++ CRUD/script.js
@@ -6,22 +6,31 @@ let productImage = document.getElementById("productImage");
 let productPrice = document.getElementById("productPrice");
 let productDescription = document.getElementById("productDescription");
 
-
 // Global State for Local Storage Data
 let setDataToLocalStorage =
   JSON.parse(localStorage.getItem("localStorageData")) || [];
 
+// Store Images in Local Storage
+let prodImage;
+productImage.addEventListener("change", (event) => {
+  const image = event.target.files[0];
+  const reader = new FileReader();
+  reader.readAsDataURL(image);
+  reader.addEventListener("load", () => {
+    prodImage = reader.result;
+  });
+});
+
 // Submit Button Event
 submitBtn.addEventListener("click", (event) => {
   event.preventDefault(); // Hold the Page after Submit button clicked
-  alert("Product Added Sucessfully..!!")
+  alert("Product Added Sucessfully..!!");
   location.replace("product.html");
-
 
   // Get values from Input
   let productIDValue = productID.value;
   let productNameValue = productName.value;
-  let productImageValue = productImage.value;
+  let productImageValue = prodImage;
   let productPriceValue = productPrice.value;
   let productDescriptionValue = productDescription.value;
 
